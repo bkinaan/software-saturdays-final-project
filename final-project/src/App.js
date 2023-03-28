9d9ca4b3e1a70419d9dfd6d4ab3458bbe4ce5919
Brandon Kinaan <89668762+bkinaan@users.noreply.github.com>
2023-03-28 17:50:02 -0400

    title

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,9 +1,14 @@
 import PokeList from './components/PokeList';
 import Team from './components/Team';
-
+import './index.css';
+ /* bg-slate-100 py-4 */
 function App() {
   return (
     <>
+    <div className='box-border p-20 bg-pokemon-landscape bg-center'>
+   
+      <h1 className='flex justify-center font-bold text-9xl'>Pokemon Database</h1>
+    </div>
     <Team />
     <PokeList />
     </>
