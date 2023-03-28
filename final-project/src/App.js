50f45117cebbc473942dc862aeb1f1ebb3db0110
Brandon Kinaan <89668762+bkinaan@users.noreply.github.com>
2023-03-28 19:14:49 -0400

    updated background

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,16 +1,24 @@
 import PokeList from './components/PokeList';
 import Team from './components/Team';
 import './index.css';
- /* bg-slate-100 py-4 */
 function App() {
   return (
     <>
+    <div
+      style={{
+        backgroundColor: "#52A4B1",
+        height: "100vh",
+        width: "100vw",
+      }}
+    >
     <div className='box-border p-20 bg-pokemon-landscape bg-center'>
-   
-      <h1 className='flex justify-center font-bold text-9xl'>Pokemon Database</h1>
+      <h1 className='flex justify-center font-bold text-9xl text-title'>Pokemon Database</h1>
     </div>
+    <div className='bg-gradient-to-br from-light-light-green to-blue'>
     <Team />
     <PokeList />
+    </div>
+    </div>
     </>
   );
 }
