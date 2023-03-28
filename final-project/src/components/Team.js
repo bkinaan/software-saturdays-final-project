a71ef348fc8d46b0fd431b16cd014a066b9d0e52
Brandon Kinaan <89668762+bkinaan@users.noreply.github.com>
2023-03-28 19:39:39 -0400

    working on firebase

--- final-project/src/components/Team.js
+++ final-project/src/components/Team.js
@@ -1,14 +1,45 @@
+import { database } from '../database.js';
+import { onValue, ref, off } from 'firebase/database';
+import React, { useEffect, useState } from 'react';
+
 function Team() {
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        const dataRef = ref(database, '/');
+        onValue(dataRef, (snap) => {
+            setData(snap.val());
+            for (let key in snap.val()) {
+                console.log(key, ':');
+                for (let sub_key in snap.val()[key]) {
+                    console.log('    ', sub_key, ':', snap.val()[key][sub_key]);
+                }
+                console.log('-----------------');
+            }
+        });
+
+        return () => {
+            const dataRef = ref(database, '/');
+            off(dataRef);
+            console.log("Removed listener");
+        }
+    }, [])
+
     return (
         <>
         <div className='
             flex
             justify-center'>
-        <h2 className='
-            text-7xl
-            font-bold
-            text-title'>
-                Team</h2>
+            <h2 className='
+                text-7xl
+                font-bold
+                text-title'>
+                    Team
+        </h2>
+        <li>
+            { data ? Object.keys(data).map((key) => <ul>{key}</ul>) : null}
+        </li>
+        
         </div>
         </>
     )
---
+++ final-project/src/database.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOrmZAF1AHaCoI96z4oHc_VKWKPPe6fnQ",
+  authDomain: "software-saturdays-final.firebaseapp.com",
+  projectId: "software-saturdays-final",
+  storageBucket: "software-saturdays-final.appspot.com",
+  messagingSenderId: "1080875423899",
+  appId: "1:1080875423899:web:909aec23e082d35fad0e3e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const database = getDatabase(app);
+
+export { database };
