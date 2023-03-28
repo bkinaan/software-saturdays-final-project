723ab8b14e1d8f7285a52e77b2669fd7eeedf7a0
Brandon Kinaan <89668762+bkinaan@users.noreply.github.com>
2023-03-28 19:14:23 -0400

    updated colors

--- final-project/tailwind.config.js
+++ final-project/tailwind.config.js
@@ -12,7 +12,8 @@ module.exports = {
       'green': '#1B3F2A',
       'light-green': '#3C7828',
       'light-light-green': '#92D79F',
-      'blue': '#52A4B1'
+      'blue': '#52A4B1',
+      'title': '#080936'
     }
   },
   plugins: [],
