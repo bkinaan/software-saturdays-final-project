da67b2bfe3deb8e35225f55333b142977b92aaba
Brandon Kinaan <89668762+bkinaan@users.noreply.github.com>
2023-03-30 14:01:46 -0400

    added create, update, read, delete for firebase (not working yet)

--- final-project/src/components/Team.js
+++ final-project/src/components/Team.js
@@ -1,9 +1,72 @@
 import { database } from '../database.js';
-import { onValue, ref, off } from 'firebase/database';
+import { set, update, onValue, ref, off, remove } from 'firebase/database';
 import React, { useEffect, useState } from 'react';
 
 function Team() {
     const [data, setData] = useState();
+
+    const createData = () => {
+        let data = {
+            key: 'value',
+            otherkey: 'other value',
+        }
+        const dataRef = ref(database, '/added');
+        set(dataRef, data)
+        .then(() => {
+            console.log("Set was successful");
+        })
+        .catch((error) => {
+            console.log("Set failed");
+            console.log(error);
+        });
+    }
+
+    const readData = () => {
+        const dataRef = ref(database, '/');
+        onValue(dataRef, (snap) => {
+            for (let key in snap.val()) {
+                console.log(key, ':');
+                for (let sub_key in snap.val()[key]) {
+                    console.log('   ', sub_key, ':', snap.val()[key][sub_key]);
+                }
+                console.log('--------------------');
+            }
+        })
+    }
+
+    const updateData = () => {
+        let data = { // TODO: replace data with pokemon fields
+            otherKey: 'new value',
+        }
+        const dataRef = ref(database, 'added'); // TODO: replace 'added' with key
+        update(dataRef, data)
+        .then(() => {
+            console.log("Update was successful");
+        })
+        .catch((error) => {
+            console.log("Update failed");
+            console.log(error);
+        });
+
+        console.log('--------------------');
+        console.log('--------------------');
+        console.log('--------------------');
+    }
+
+    const deleteData = () => {
+        const dataRef = ref(database, '/added'); // TODO: replace 'added' with key
+        remove(dataRef)
+        .then(() => {
+            console.log("Remove was successful");
+        })
+        .catch((error) => {
+            console.log("Remove failed");
+            console.log(error);
+        });
+        console.log('--------------------');
+        console.log('--------------------');
+        console.log('--------------------');
+    }
 
     useEffect(() => {
         const dataRef = ref(database, '/');
@@ -36,10 +99,15 @@ function Team() {
                 text-title'>
                     Team
         </h2>
-        <li>
-            { data ? Object.keys(data).map((key) => <ul>{key}</ul>) : null}
-        </li>
-        
+        <div>
+            { data ? Object.keys(data).map((key) => <div>{key}</div>) : null}
+        </div>
+        <div>
+        <button onClick={readData}>Read Data</button><br /><br />
+            <button onClick={createData}>Create Data</button><br /><br />
+            <button onClick={updateData}>Update Data</button><br /><br />
+            <button onClick={deleteData}>Delete Data</button><br /><br />
+            </div>
         </div>
         </>
     )
