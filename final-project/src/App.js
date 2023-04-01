bc8eb67db88980d7eba23816c31c4d477db1abd6
Brandon Kinaan <89668762+bkinaan@users.noreply.github.com>
2023-04-01 14:09:37 -0400

    moving states to parent component

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,7 +1,28 @@
 import PokeList from './components/PokeList';
 import Team from './components/Team';
+import React, { useState } from 'react';
 import './index.css';
 function App() {
+  const [list, setList] = useState([]);
+  const [count, setCount] = useState(0);
+
+  const addToList = (name) => {
+    setList([...list, name]);
+  }
+
+  const removeFromList = (name) => {
+    setList(list.filter(item => item !== name));
+    setCount(count - 1);
+  }
+
+  const updateCount = (count) => {
+    setCount(count);
+  }
+
+  const updateList = (list) => {
+    setList(list);
+  }
+  
   return (
     <>
     <div
@@ -15,8 +36,8 @@ function App() {
       <h1 className='flex justify-center font-bold text-9xl text-title'>Pokemon Database</h1>
     </div>
     <div className='bg-gradient-to-br from-light-light-green to-blue'>
-    <Team />
-    <PokeList />
+    <Team count={count} list={list} removeFromList={removeFromList} updateCount={updateCount} updateList={updateList}/>
+    <PokeList count={count} list={list} addToList={addToList} />
     <div
         className='text-right pr-4 pb-4'
       >Source Code on <a className='underline' href='https://github.com/bkinaan/software-saturdays-final-project'>GitHub</a></div>
