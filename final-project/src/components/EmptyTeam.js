63e2488435bb53220b3adade5f0ba6aab2fba6e4
Brandon Kinaan <89668762+bkinaan@users.noreply.github.com>
2023-04-01 01:37:42 -0400

    message if there is no pokemon in team

---
+++ final-project/src/components/EmptyTeam.js
@@ -0,0 +1,11 @@
+function EmptyTeam() {
+    return (
+        <>
+            <h1
+                className='col-span-full pb-10 text-3xl'
+            >There's currently no pokemon in your team. Add some below!</h1>
+        </>
+    )
+}
+
+export default EmptyTeam;
